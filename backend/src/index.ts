f23d56ebf0bf6a578abd33d3f490ec814526cbcb
Julianna Iodo <julianna.i@firebirdtours.com>
2026-07-02 13:27:00 +0300

    feat: created index.ts

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { autoSaveProfile } from './controllers/profile.controller';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "CV Management API работает!" });
+});
+
+app.post('/api/profile/autosave', autoSaveProfile);
+
+app.listen(PORT, () => {
+    console.log(`Сервер успешно запущен на порту ${PORT}`);
+});
